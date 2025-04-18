5cfe4adb4cd12383b7b5dd10800a191ed2d91fac
Minhajul Islam <mmminhaj221@gmail.com>
2025-04-18 09:40:41 +0600

    added get all course basic end point

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var mongoClient = new MongoClient("mongodb+srv://testminhaj:yzsKdGz9zfWwhNu8@owncluster.4skch.mongodb.net/?retryWrites=true&w=majority&appName=OwnCluster");
+var database = mongoClient.GetDatabase("unishare");
+
+app.MapGet("/", async () =>
+{
+    try
+    {
+        var collection = database.GetCollection<BsonDocument>("courses");
+        var documents = await collection.Find(new BsonDocument()).ToListAsync();
+
+        // Convert all BsonDocuments to clean JSON objects
+        var jsonList = documents.Select(doc => BsonTypeMapper.MapToDotNetValue(doc)).ToList();
+
+        return Results.Json(new { courses = jsonList });
+    }
+    catch (Exception ex)
+    {
+        return Results.Json(new { error = ex.Message }, statusCode: 500);
+    }
+});
 
 app.Run();
+
